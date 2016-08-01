fd5d68b402dffd698d0cb0d37f749ede8fb65b07
Popescu Radu <popescustefanradu@gmail.com>
2016-08-01 11:33:37 +0300

    Created base impl, Server modified to ext PluginBase

---
+++ src/ImapPluginManager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\radu_imap;
+
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Plugin\DefaultPluginManager;
+
+class ImapPluginManager extends DefaultPluginManager {
+
+  /**
+   * Creates the discovery object.
+   *
+   * @param \Traversable $namespaces
+   *   An object that implements \Traversable which contains the root paths
+   *   keyed by the corresponding namespace to look for plugin implementations.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cacheBackend
+   *   Cache backend instance to use.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
+   */
+  public function __construct(\Traversable $namespaces, CacheBackendInterface $cacheBackend, ModuleHandlerInterface $moduleHandler) {
+    $subdir = 'Plugin/Imap';
+    $pluginInterface = 'Drupal\radu_imap\ImapInterface';
+    $pluginDefinitionAnotationName = 'Drupal\Component\Annotation\Plugin';
+
+    parent::__construct($subdir,$namespaces,$moduleHandler,$pluginInterface,$pluginDefinitionAnotationName);
+
+//    $this->alterInfo('imap_info');
+//    $this->setCacheBackend($cacheBackend,'sandwich_info');
+  }
+
+}
